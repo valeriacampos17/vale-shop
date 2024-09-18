891b287ad5f3a9c2e8c5b516462722231948305e
Valeria Campos <camposrevillavaleria@gmail.com>
2024-09-18 10:05:25 -0400

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+// Theme toggle functionality
+document.getElementById('theme-toggle').addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+    const icon = document.querySelector('#theme-toggle i');
+    if (document.body.classList.contains('dark-theme')) {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+    } else {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const navItems = document.querySelectorAll('.nav-item');
+    const currentPage = window.location.pathname.split('/').pop();
+
+    navItems.forEach(item => {
+        const href = item.getAttribute('href');
+
+        item.classList.remove('active');
+        
+        if (href === currentPage) {
+            item.classList.add('active');
+        }
+    });
+});
